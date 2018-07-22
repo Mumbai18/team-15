01e2c69ed16aaf330bc2bb880c5314c4c7b67bd7
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2018-07-22 11:24:12 +0530

    Final Commit

---
+++ webapp/firebase.js
@@ -0,0 +1,13 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAQjXrIlKefX8jHBG8owE0QU8FeCOOUSiA",
+    authDomain: "vcare-bb222.firebaseapp.com",
+    databaseURL: "https://vcare-bb222.firebaseio.com",
+    projectId: "vcare-bb222",
+    storageBucket: "vcare-bb222.appspot.com",
+    messagingSenderId: "416307041911"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.firestore()
